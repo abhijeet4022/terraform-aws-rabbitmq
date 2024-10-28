4dc3e78e0967255d114bf6049cee50afdf24110c
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-28 16:59:55 +0530

    Project Infra

--- userdata.sh
+++ userdata.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo yum install ansible -y &> /opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible.git main.yml -e component=rabbitmq &> /opt/userdata.log
