09e8ac3255fb122808df6601a01a97eed41ae5a8
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-28 17:13:37 +0530

    Project Infra

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sudo yum install ansible -y &> /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible.git main.yml -e component=rabbitmq &> /opt/userdata.log
+
+# Install Ansible and redirect both stdout and stderr to userdata.log
+sudo yum install ansible -y &>> /opt/userdata.log
+
+# Run ansible-pull to pull the playbook and redirect both stdout and stderr to userdata.log
+ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible.git main.yml -e component=rabbitmq &>> /opt/userdata.log
