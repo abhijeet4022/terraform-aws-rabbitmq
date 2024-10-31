d845b705e708751895149ca3b2a5e5d3b0a5f66e
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-31 16:26:10 +0530

    Project App

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 # Install Ansible and redirect both stdout and stderr to userdata.log
-sudo yum install ansible -y &>> /opt/userdata.log
+sudo yum install ansible python3.12-pip -y &> /opt/userdata.log
+
+# Install the python libraries to fetch the SSM Parameters.
+sudo pip3.12 install  botocore boto3 &>> /opt/userdata.log
 
 # Run ansible-pull to pull the playbook and redirect both stdout and stderr to userdata.log
-ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible.git main.yml -e component=rabbitmq &>> /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible-deployment.git main.yml -e component=rabbitmq &>> /opt/userdata.log
