ad81992d836000c14eba1dd25c141db2822cafa5
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-01 08:25:39 +0530

    Project App

--- userdata.sh
+++ userdata.sh
@@ -6,4 +6,4 @@ sudo yum install ansible python3.12-pip -y &> /opt/userdata.log
 sudo pip3.12 install  botocore boto3 &>> /opt/userdata.log
 
 # Run ansible-pull to pull the playbook and redirect both stdout and stderr to userdata.log
-ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible-deployment.git main.yml -e component=rabbitmq &>> /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/abhijeet4022/project-ansible-deployment.git main.yml -e env=dev -e component=rabbitmq &>> /opt/userdata.log
