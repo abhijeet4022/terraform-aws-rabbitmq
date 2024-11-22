163f419bd35dbd193963c106c6490ff844c60ac2
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-11-22 13:49:05 +0530

    Dairy Notes - Updated on 22 Nov.

--- main.tf
+++ main.tf
@@ -84,12 +84,16 @@ resource "aws_iam_policy" "main" {
         "Sid" : "VisualEditor0",
         "Effect" : "Allow",
         "Action" : [
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:060795929502:parameter/payment.${var.env}.*"
+        "Resource": [
+          "arn:aws:ssm:us-east-1:060795929502:parameter/payment.dev.*",
+          "arn:aws:kms:us-east-1:060795929502:key/44d09148-0677-4a8b-a183-cd2a9b03b2eb"
+        ]
       },
       {
         "Sid" : "VisualEditor1",
